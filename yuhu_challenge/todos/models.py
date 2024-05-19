d0af01f1691560f911172c1bcd3d658d7647ce98
aHardReset <ing.aarongaribay@outlook.com>
2024-05-18 17:31:19 -0700

    adding blank posibility to due date

--- yuhu_challenge/todos/models.py
+++ yuhu_challenge/todos/models.py
@@ -6,7 +6,7 @@ class ToDoItem(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.CharField(max_length=500, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    due_date = models.DateTimeField(default=None, null=True)
+    due_date = models.DateTimeField(default=None, blank=True, null=True)
     is_done = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
